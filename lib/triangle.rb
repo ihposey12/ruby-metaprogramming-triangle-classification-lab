f796acfc44fb13956683fe784a857e56be7f588e
Hunter Posey <ihposey@crimson.ua.edu>
2020-12-01 18:27:03 -0800

    final procedure

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    valid_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def valid_triangle
+    is_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each do |side|
+      is_triangle << false if side <= 0
+    raise TriangleError if is_triangle.include?(false)
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
